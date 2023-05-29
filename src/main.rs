25abc7fc15a148cf9cf760cb365a73658d76ea18
Billy Zhang <bz6639081@gmail.com>
2023-05-29 14:54:14 -0400

    Connect with a postgres database

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,39 @@
-use actix_web::{web, App, HttpServer};
+#[macro_use]
+extern crate diesel;
+
+use actix_web::{dev::ServiceRequest, web, App, Error, HttpServer};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
 
 mod handlers;
+mod schema;
+mod errors;
+mod models;
+
+pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 #[actix_rt::main]
 
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    std::env::set_var("RUST_LOG", "actix_web=debug");
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    // create db connection pool
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool: Pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+
     std::env::set_var("RUST_LOG", "actix_web=debug");
 
     HttpServer::new(move || {
         App::new()
-        .route("/users", web::get().to(handlers::get_users))
-        .route("/users/{id}", web::get().to(handlers::get_user_by_id))
-        .route("/users", web::post().to(handlers::add_user))
-        .route("/users/{id}", web::post().to(handlers::delete_user))
+            .data(pool.clone())
+            .route("/users", web::get().to(handlers::get_users))
+            .route("/users/{id}", web::get().to(handlers::get_user_by_id))
+            .route("/users", web::post().to(handlers::add_user))
+            .route("/users/{id}", web::post().to(handlers::delete_user))
     })
     .bind("127.0.0.1:8080")?
     .run()
